844a9b09b4099551a256f9f7493cc3e89f50e07c
Franco Sosuan <franco.sosuan@gmail.com>
2019-03-27 12:00:21 +0800

    Given a .txt file that has a list of a bunch of names, count how many of each name there are in the file, and print out the results to the screen. I have a .txt file for you, if you want to use it!

--- 2-ReadFromFile.py
+++ 2-ReadFromFile.py
@@ -1,19 +1,18 @@
-
 counter_dict = {}
 
 with open('nameslist.txt') as f:
 
-	line = f.readline()
+    line = f.readline()
 
     while line:
 
-		line = line.strip()
+        line = line.strip()
 
         if line in counter_dict:
             counter_dict[line] += 1
 
-		else:
-			counter_dict[line] = 1
-		line = f.readline()
+        else:
+            counter_dict[line] = 1
+        line = f.readline()
 
 print(counter_dict)
